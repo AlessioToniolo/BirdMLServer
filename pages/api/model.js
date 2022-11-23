50c6e4599762a9c40a32ac37b8bca188ac5b3825
AlessioToniolo <atoniolo76@gmail.com>
2022-11-23 11:12:51 -0500

    Model.json API Response

---
+++ pages/api/model.js
@@ -0,0 +1,21 @@
+import * as path from 'path';
+import * as fileSystem from 'fs';
+
+export default async function handler(req, res) {
+    //var filePath = path.join(__dirname, 'model.json');
+    const filePath = './pages/api/model.json'
+    var stat = fileSystem.statSync(filePath);
+
+    res.writeHead(200, {
+        'Content-Type': 'application/json', // try text/plain if doesn't work
+        'Content-Length': stat.size
+    });
+
+    var readStream = fileSystem.createReadStream(filePath);
+    
+    await new Promise(function (resolve) {
+        readStream.pipe(res)
+        readStream.on('end', resolve)
+      });
+}
+  
