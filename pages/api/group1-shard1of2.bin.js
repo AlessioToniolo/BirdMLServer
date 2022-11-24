03d0c1add4854b7c57127d2957f41129b1f61036
AlessioToniolo <atoniolo76@gmail.com>
2022-11-23 22:50:23 -0500

    Enable CORS & Add Weight File Fetching

---
+++ pages/api/group1-shard1of2.bin.js
@@ -0,0 +1,20 @@
+import * as path from 'path';
+import * as fileSystem from 'fs';
+
+export default async function handler(req, res) {
+    //var filePath = path.join(__dirname, 'model.json');
+    const filePath = path.join(process.cwd(), 'public/group1-shard1of2.bin');
+    var stat = fileSystem.statSync(filePath);
+
+    res.writeHead(200, {
+        'Content-Type': 'application/octet-stream', // try text/plain if doesn't work
+        'Content-Length': stat.size
+    });
+
+    var readStream = fileSystem.createReadStream(filePath);
+    
+    await new Promise(function (resolve) {
+        readStream.pipe(res)
+        readStream.on('end', resolve)
+      });
+}
