9771d870588f967c029a4652476d1463931afcba
AlessioToniolo <atoniolo76@gmail.com>
2022-12-04 11:25:27 -0500

    Backup

---
+++ pages/api/logging.js
@@ -0,0 +1,17 @@
+import { createClient } from '@supabase/supabase-js';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export default async function handler(req, res) {
+    const body = req.body;
+    const supabase = createClient(process.env.SUPABASE_PROJECT_URL, process.env.SUPABASE_API_KEY);
+    const { data, error } = await supabase
+        .from('birds')
+        .insert([{ id: 1, bird_species: body}]);
+
+    if (error) {throw new Error(error.message)}
+    else {
+
+    }
+
+    res.status(200).json({ text: 'Hello' });
